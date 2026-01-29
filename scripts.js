5e739327f2a3d5bc8c777f47e397dd440a4c838d
archishoyo <rb5558645@gmail.com>
2026-01-29 05:35:29 -0400

    Each click should call the function

--- scripts.js
+++ scripts.js
@@ -3,3 +3,13 @@ const gameRNG = () => Math.floor(Math.random() * 3);
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
+
+rock.addEventListener("click", () => {
+  gameRNG();
+})
+paper.addEventListener("click", () => {
+  gameRNG();
+})
+scissors.addEventListener("click", () => {
+  gameRNG();
+})
