519b573e0d543447f6551a6d18dcf6dc2f5dda16
archishoyo <rb5558645@gmail.com>
2026-01-29 05:33:52 -0400

    Select DOM elements for player choices

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const gameRNG = () => Math.floor(Math.random() * 3);
+
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
