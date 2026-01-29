5d81360757b6cdc2ec48f376fc346e45c4aba2d4
archishoyo <rb5558645@gmail.com>
2026-01-29 05:37:41 -0400

    Clicking generates CPU choice and player choice

--- scripts.js
+++ scripts.js
@@ -4,12 +4,18 @@ const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 
+let playerChoice;
+let computerChoice;
+
 rock.addEventListener("click", () => {
-  gameRNG();
+  computerChoice = gameRNG();
+  playerChoice = rock;
 })
 paper.addEventListener("click", () => {
-  gameRNG();
+  computerChoice = gameRNG();
+  playerChoice = paper;
 })
 scissors.addEventListener("click", () => {
-  gameRNG();
+  computerChoice = gameRNG();
+  playerChoice = scissors;
 })
